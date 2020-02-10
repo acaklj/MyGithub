fb971cf357e71fc650e5410dcdfe5afcc12e1c94
Kevin <acaklj@naver.com>
2020-02-10 10:29:43 +0900

    수정....패치

--- app/src/main/java/com/ezen/mygithub/MainActivity.java
+++ app/src/main/java/com/ezen/mygithub/MainActivity.java
@@ -6,12 +6,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    //소스 수정
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
     }
 }
